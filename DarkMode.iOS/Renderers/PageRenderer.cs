2c03c572a3fc930888f2d479a078d10d81e558d8
Eduardo Rosas <lalorosas@outlook.com>
2019-09-24 10:06:53 -0500

    Initial commit

---
+++ DarkMode.iOS/Renderers/PageRenderer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using DarkMode.Styles;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ContentPage), typeof(DarkMode.iOS.Renderers.PageRenderer))]
+namespace DarkMode.iOS.Renderers
+{
+    public class PageRenderer : Xamarin.Forms.Platform.iOS.PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.OldElement != null || Element == null)
+                return;
+
+
+            try
+            {
+                SetTheme();
+            }
+            catch(Exception ex) { }
+        }
+
+        public override void TraitCollectionDidChange(UITraitCollection previousTraitCollection)
+        {
+            base.TraitCollectionDidChange(previousTraitCollection);
+
+            if (TraitCollection.UserInterfaceStyle != previousTraitCollection.UserInterfaceStyle)
+                SetTheme();
+        }
+
+        private void SetTheme()
+        {
+            if (TraitCollection.UserInterfaceStyle == UIUserInterfaceStyle.Dark)
+                App.Current.Resources = new DarkTheme();
+            else
+                App.Current.Resources = new LightTheme();
+        }
+    }
+}
